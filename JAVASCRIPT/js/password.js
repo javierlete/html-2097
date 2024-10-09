2808cf2538cf4c69e6de5bac6ae06923f74f6798
Javier Lete <javier@eskura21.com>
2024-10-09 09:25:00 +0200

    Mostrar password inicial

---
+++ JAVASCRIPT/js/password.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const boton = document.querySelector('button');
+const input = document.querySelector('input');
+
+console.log(boton);
+console.log(input);
+
+input.placeholder = 'Pulsa botón para ver password';
+
+boton.addEventListener('click', mostrarPassword);
+
+function mostrarPassword() {
+    console.log('CLICK');
+
+    console.log(input.type);
+
+    input.type = 'text';
+}

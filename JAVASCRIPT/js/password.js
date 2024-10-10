41ebefe6aaa627b2c8a9c5e7bd3fd08675f4e501
Javier Lete <javier@eskura21.com>
2024-10-10 11:10:46 +0200

    Ahora el color de los botones cambia según la validación

--- JAVASCRIPT/js/password.js
+++ JAVASCRIPT/js/password.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const form = document.forms[0];
 const inputsPassword = document.querySelectorAll('input[type=password]');
+
+form.addEventListener('submit', () => setTimeout(cambiarColores, 0));
 
 console.log(inputsPassword);
 
@@ -9,7 +12,7 @@ for (const inputPassword of inputsPassword) {
 
     inputPassword.placeholder = 'Pulsa botón para ver password';
     const boton = document.createElement('button');
-    boton.className = 'btn border border-start-0 bi bi-eye';
+    boton.className = 'btn border border-start-0 rounded-end bi bi-eye';
     boton.type = 'button';
 
     console.log(boton);
@@ -18,6 +21,8 @@ for (const inputPassword of inputsPassword) {
     inputPassword.parentElement.classList.add('input-group');
 
     inputPassword.after(boton);
+
+    inputPassword.addEventListener('input', cambioPassword);
 
     boton.addEventListener('click', mostrarOcultarPassword);
 }
@@ -39,7 +44,34 @@ function mostrarOcultarPassword(evento) {
         boton.classList.replace('bi-eye', 'bi-eye-slash');
     } else {
         inputPassword.type = 'password';
-        
+
         boton.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+}
+
+function cambiarColores() {
+    console.log('CAMBIAR COLORES');
+    
+    if (form.classList.contains('was-validated')) {
+        for (const inputPassword of inputsPassword) {
+            colorearBoton(inputPassword);
+        }
+    }
+}
+
+function cambioPassword(evento) {
+    const input = evento.target;
+    colorearBoton(input);
+}
+
+function colorearBoton(input) {
+    const boton = input.nextElementSibling;
+
+    boton.classList.remove('border-success', 'text-success', 'border-danger', 'text-danger');
+
+    if (input.validity.valid) {
+        boton.classList.add('border-success', 'text-success');
+    } else {
+        boton.classList.add('border-danger', 'text-danger');
     }
 }
